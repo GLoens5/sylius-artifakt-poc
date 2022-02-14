884450fb78aaade98e665fdacf426f1c7e658e40
gloens <gloens@siliconsalad.com>
2022-02-14 16:59:50 +0100

    add .env

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -5,10 +5,10 @@ set -e
 echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
 
 # used by entrypoint to init JWT
-sudo apt-get update && \
-    sudo apt-get install -y --no-install-recommends acl && \
-    sudo rm -rf /var/lib/apt/lists/*
+apt-get update && \
+    apt-get install -y --no-install-recommends acl && \
+    rm -rf /var/lib/apt/lists/*
 
-sudo chmod 755 /var/www/html/bin/console
+chmod 755 /var/www/html/bin/console
 
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
